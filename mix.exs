8a2d5138887b6280b685e19124c2fb92472cf49c
v0idpwn <v0idpwn@gmail.com>
2024-09-18 10:50:26 -0300

    Release 1.0.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ConcurrencyLimiter.MixProject do
   use Mix.Project
 
-  @version "1.0.0"
+  @version "1.0.1"
   @url "https://github.com/coingaming/concurrency_limiter"
 
   def project do
